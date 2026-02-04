51eaecd202f123e8df5eb80ec1885f4985fbf4a1
Matias Campos <167012454+ignacamposs@users.noreply.github.com>
2026-02-04 19:39:48 -0300

    Delete Zona-autos directory

--- Zona-autos/index.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const navLinks = document.querySelector('.nav-links');
-menuToggle.addEventListener('click', () => {
-        // 🔄 Esta línea agrega la clase 'active' si no está, y la quita si ya está
-        navLinks.classList.toggle('active');
- });
